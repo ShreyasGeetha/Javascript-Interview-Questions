155a38b7783430ced5c5d54c9303d6d66c201c03
ShreyasGeetha <shreyasgeetha@gmail.com>
2022-02-12 14:42:17 +0530

    polyfillForMap

--- polyfill in Javascript/2 polyfillForMap.js
+++ polyfill in Javascript/2 polyfillForMap.js
@@ -2,14 +2,31 @@
  * Write your own implementation for Array.map function
  */
 
-let arr = [1, 2, 3, 4]
-
-Array.prototype.myMap = function () {
-  let arr = []
+/**
+ * 
+ * @param {*} callback - here callback has 3 arugments, 
+ * a. value of the array
+ * b. index of the element
+ * c. the array itself 
+ * @returns 
+ */
+Array.prototype.newMap = function (callback) {
+  let result = []
+  //this holds the array chars
   for (let i = 0; i < this.length; i++){
-    arr.push(this[i])
+    if (this.indexOf(this[i]) > -1) {      
+      //here control goes back to line 26, executes line 27 and returns the control back here and append the manipulated value to result array
+      result[i] = callback(this[i], i, this)
+    }
   }
-  return arr
+  return result;
 }
 
-arr.myMap((val,index)=>console.log('hello ',val))
+// sample array
+const chars = ['a','b','c','d']
+chars[10] = 'j';
+
+//double will be assigned only after all the array are iterated through
+const double = chars.newMap((item, index) => {
+  console.log(item+' alphabet')
+})
